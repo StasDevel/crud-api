430adc719ab1d02792926ff30c089872e87fd23c
Stanislav Shkradov <studingDev@gmail.com>
2024-10-21 21:43:38 +0300

    fix: fixed routes

--- src/app.ts
+++ src/app.ts
@@ -7,32 +7,30 @@ const { correctUser } = require("./controllers/userController");
 const { deleteUser } = require("./controllers/userController");
 
 exports.app = (req: IncomingMessage, res: ServerResponse) => {
-  switch (req.method) {
-    case "GET": {
-      if (req.url === "/api/users") {
-        showUsers(req, res);
-      }
-      if (req.url.startsWith("/api/users")) {
-        showConstUser(req, res);
-      }
-    }
+  const url = req.url;
 
-    case "POST": {
-      if (req.url === "/api/users") {
-        createNewUser(req, res);
-      }
+  if (url === "/api/users") {
+    if (req.method === "GET") {
+      showUsers(req, res);
+    } else if (req.method === "POST") {
+      createNewUser(req, res);
+    } else {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Route wasn't found" }));
     }
-
-    case "PUT": {
-      if (req.url.startsWith("/api/users")) {
-        correctUser(req, res);
-      }
+  } else if (url?.startsWith("/api/users/")) {
+    if (req.method === "GET") {
+      showConstUser(req, res);
+    } else if (req.method === "PUT") {
+      correctUser(req, res);
+    } else if (req.method === "DELETE") {
+      deleteUser(req, res);
+    } else {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Route wasn't found" }));
     }
-
-    case "DELETE": {
-      if (req.url.startsWith("/api/users")) {
-        deleteUser(req, res);
-      }
-    }
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Route wasn't found" }));
   }
 };
